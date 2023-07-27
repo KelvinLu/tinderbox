90e1d88f17884a7f0939be901aae843ceb05c92f
Kelvin Lu <kelvinlu4@gmail.com>
2023-07-26 21:04:51 -0700

    $CONFIGURATION_DIR

--- lib/configuration/configuration.rb
+++ lib/configuration/configuration.rb
@@ -12,7 +12,11 @@ module Tinderbox::Configuration
     @configuration = nil
 
     def file(filepath)
-      filepath = File.join(File.dirname(__FILE__), '../../configuration', filepath)
+      configuration_dir = ENV['CONFIGURATION_DIR'] || File.join(
+        File.dirname(__FILE__), '../../configuration'
+      )
+
+      filepath = File.join(configuration_dir, filepath)
       raise "file at #{filepath} not found" unless File.exist?(filepath)
 
       @filepath = filepath
